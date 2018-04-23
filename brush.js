3fd2637279001b841f5e9638895f931af9d5f2f9
Leonid Vlasov <leoniv.vlasov@gmail.com>
2018-04-24 00:43:03 +0700

    problem with utf-8 keywords

--- brush.js
+++ brush.js
@@ -3,22 +3,32 @@ var regexLib = require('syntaxhighlighter-regex').commonRegExp;
 
 function Brush() {
  var keywords = 'КонецПроцедуры EndProcedure КонецФункции EndFunction'+
-                'Прервать Break Продолжить Continue' +
-                'Возврат Return Если If' +
-                'Иначе Else ИначеЕсли ElsIf' +
-                'Тогда Then КонецЕсли EndIf' +
-                'Попытка Try Исключение Except' +
-                'КонецПопытки EndTry Raise ВызватьИсключение' +
-                'Пока While Для For' +
-                'Каждого Each Из In' +
-                'По To Цикл Do' +
-                'КонецЦикла EndDo НЕ NOT' +
-                'И AND ИЛИ OR' +
-                'Новый New Процедура Procedure' +
-                'Функция Function Перем Var' +
-                'Экспорт Export Знач Val'
+                ' Прервать Break Продолжить Continue' +
+                ' Возврат Return Если If' +
+                ' Иначе Else ИначеЕсли ElsIf' +
+                ' Тогда Then КонецЕсли EndIf' +
+                ' Попытка Try Исключение Except' +
+                ' КонецПопытки EndTry Raise ВызватьИсключение' +
+                ' Пока While Для For' +
+                ' Каждого Each Из In' +
+                ' По To Цикл Do' +
+                ' КонецЦикла EndDo НЕ NOT' +
+                ' И AND ИЛИ OR' +
+                ' Новый New Процедура Procedure' +
+                ' Функция Function Перем Var' +
+                ' Экспорт Export Знач Val'
 
   var builds = 'Неопределено Undefined Истина True Ложь False NULL'
+
+  // \b - word boundary doesn't work with utf-8
+  this.getKeywordsUTF8 = function(str){
+    const results = str
+      .replace(/^\s+|\s+$/g, '')
+      .replace(/\s+/g, '|');
+
+    const _b = '[^0-9a-zA-Zа-яА-Я\.#&()]|=|^|$'
+    return "("+_b+")(?:"+results+")(?:"+_b+")"
+  }
 
   this.regexList = [
     {
@@ -42,11 +52,11 @@ function Brush() {
       css: 'value'
     },
     {
-      regex: new RegExp(this.getKeywords(keywords), 'gmi'),
+      regex: new RegExp(this.getKeywordsUTF8(keywords), 'gmi'),
       css: 'keyword bold'
     },
     {
-      regex: new RegExp(this.getKeywords(builds), 'gmi'),
+      regex: new RegExp(this.getKeywordsUTF8(builds), 'gmi'),
       css: 'value bold'
     }
     ];
